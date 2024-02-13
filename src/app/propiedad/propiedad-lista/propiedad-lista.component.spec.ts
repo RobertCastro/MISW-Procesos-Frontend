ad92a87d0f671b08f19777572cf20473f7601516
DaGamez <da.gamez96@uniandes.edu.co>
2024-02-12 22:10:18 -0500

    PA_8: nombre es la unica columna (rojo)

--- src/app/propiedad/propiedad-lista/propiedad-lista.component.spec.ts
+++ src/app/propiedad/propiedad-lista/propiedad-lista.component.spec.ts
@@ -1,1 +1,94 @@
-//TODO
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PropiedadListaComponent } from './propiedad-lista.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { ToastrService, ToastrModule } from 'ngx-toastr';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { EncabezadoAppModule } from 'src/app/encabezado-app/encabezado-app.module';
+
+describe('PropiedadListaComponent', () => {
+  let component: PropiedadListaComponent;
+  let fixture: ComponentFixture<PropiedadListaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PropiedadListaComponent],
+      imports: [ToastrModule.forRoot(),HttpClientTestingModule,EncabezadoAppModule],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {
+            paramMap: {
+              get: () => '123' // Example value for paramMap
+            }
+          }
+        }
+      },
+      ToastrService
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PropiedadListaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have "Nombre" column', () => {
+    const headerRows = fixture.nativeElement.querySelectorAll('th');
+    let direccionColumnExists = false;
+  
+    headerRows.forEach(header => {
+      if (header.textContent.trim() === 'Nombre') {
+        direccionColumnExists = true;
+      }
+    });
+  
+    expect(direccionColumnExists).toBeTrue();
+  });
+
+  it('should not have "Direccion" column', () => {
+    const headerRows = fixture.nativeElement.querySelectorAll('th');
+    let direccionColumnExists = false;
+  
+    headerRows.forEach(header => {
+      if (header.textContent.trim() === 'Dirección') {
+        direccionColumnExists = true;
+      }
+    });
+  
+    expect(direccionColumnExists).toBeFalse();
+  });
+
+  it('should not have "Nombre propietario" column', () => {
+    const headerRows = fixture.nativeElement.querySelectorAll('th');
+    let direccionColumnExists = false;
+  
+    headerRows.forEach(header => {
+      if (header.textContent.trim() === 'Nombre propietario') {
+        direccionColumnExists = true;
+      }
+    });
+  
+    expect(direccionColumnExists).toBeFalse();
+  });
+
+  it('should not have "Contacto" column', () => {
+    const headerRows = fixture.nativeElement.querySelectorAll('th');
+    let direccionColumnExists = false;
+  
+    headerRows.forEach(header => {
+      if (header.textContent.trim() === 'Contacto') {
+        direccionColumnExists = true;
+      }
+    });
+  
+    expect(direccionColumnExists).toBeFalse();
+  });
+
+});
