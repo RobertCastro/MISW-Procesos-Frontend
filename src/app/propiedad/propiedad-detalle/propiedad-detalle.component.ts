ca2ec7f04fa80b7d0061fa797f4ae6a5a093ea2e
Robert Castro <ra.castro2@uniandes.edu.co>
2024-02-14 14:24:08 -0500

    PA_9: Listado de datos en detalle de propiedad

--- src/app/propiedad/propiedad-detalle/propiedad-detalle.component.ts
+++ src/app/propiedad/propiedad-detalle/propiedad-detalle.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit,  Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PropiedadService } from '../propiedad.service';
+import { Propiedad } from '../propiedad';
 
 @Component({
   selector: 'app-propiedad-detalle',
@@ -6,19 +8,21 @@ import { Component, OnInit,  Input } from '@angular/core';
   styleUrls: ['./propiedad-detalle.css']
 })
 export class PropiedadDetalleComponent implements OnInit {
-
-
-  constructor() { }
-
   @Input() propiedadId: number;
+  propiedad: Propiedad;
+
+  constructor(private propiedadService: PropiedadService) { }
 
   ngOnInit(): void {
-    this.cargarDetallePropiedad(this.propiedadId);
+    if (this.propiedadId) {
+      this.cargarDetallePropiedad(this.propiedadId);
+    }
   }
 
   cargarDetallePropiedad(id: number) {
-    // return this.http.get<Propiedad>(`/api/propiedades/${id}`);
+    this.propiedadService.darPropiedad(id).subscribe({
+      next: (data) => this.propiedad = data,
+      error: (error) => console.error('Error al obtener los detalles de la propiedad', error)
+    });
   }
-  
-
 }
