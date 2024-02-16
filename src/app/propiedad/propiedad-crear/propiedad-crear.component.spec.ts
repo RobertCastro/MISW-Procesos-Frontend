3c763c9b20686f4bf4110cb57ed126a3b6c39cf6
mar-alas <m.alas@uniandes.edu.co>
2024-02-15 22:34:12 -0800

    PA_11 Unit Test

--- src/app/propiedad/propiedad-crear/propiedad-crear.component.spec.ts
+++ src/app/propiedad/propiedad-crear/propiedad-crear.component.spec.ts
@@ -9,6 +9,7 @@ import { ToastrModule } from 'ngx-toastr';
 import {HttpClientModule} from '@angular/common/http';
 import { EncabezadoAppModule } from 'src/app/encabezado-app/encabezado-app.module';
 
+
 describe('PropiedadCrearComponent', () => {
   let component: PropiedadCrearComponent;
   let fixture: ComponentFixture<PropiedadCrearComponent>;
@@ -30,7 +31,97 @@ describe('PropiedadCrearComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create the component crear propiedad', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should have button Submit with test_id="bot_submit"', () => {
+    const button = fixture.debugElement.query(By.css('[test-id="bot_submit"]'));
+    expect(button).toBeTruthy();
+  });
+
+  it('Should have button Cencel with test_id="bot_cancel"', () => {
+    const button = fixture.debugElement.query(By.css('[test-id="bot_cancel"]'));
+    expect(button).toBeTruthy();
+  });
+
+  it('Should have a Nombre Propiedad text box', () => {
+    const name: HTMLElement = fixture.nativeElement.querySelector('#nombre_propiedad');
+    expect(name).toBeTruthy();
+  });
+
+  it('should display error message when Nombre Propiedad field is touched and empty', () => {
+    const inputElement = fixture.debugElement.query(By.css('#nombre_propiedad'));
+    inputElement.nativeElement.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.alert.alert-danger'));
+    expect(errorMessage.nativeElement.textContent.trim()).toBe('Contenido requerido');
+  });
+
+  it('Should have a ciudad text box', () => {
+    const name: HTMLElement = fixture.nativeElement.querySelector('#ciudad');
+    expect(name).toBeTruthy();
+  });
+
+  it('should display error message when Ciudad field is touched and empty', () => {
+    const inputElement = fixture.debugElement.query(By.css('#ciudad')); // Selecting the input element
+    inputElement.nativeElement.dispatchEvent(new Event('blur')); // Simulating blur event
+    fixture.detectChanges(); // Updating the component
+
+    const errorMessage = fixture.debugElement.query(By.css('.alert.alert-danger')); // Selecting the error message element
+    expect(errorMessage).toBeTruthy(); // Asserting that the error message exists
+    expect(errorMessage.nativeElement.textContent.trim()).toBe('Por favor diligencie este campo'); // Asserting the content of the error message
+  });
+
+  it('Should have a municipio text box', () => {
+    const name: HTMLElement = fixture.nativeElement.querySelector('#municipio');
+    expect(name).toBeTruthy();
+  });
+
+  it('Should have a direccion text box', () => {
+    const name: HTMLElement = fixture.nativeElement.querySelector('#direccion');
+    expect(name).toBeTruthy();
+  });
+
+  it('should display error message when Dirección field is touched and empty', () => {
+    const inputElement = fixture.debugElement.query(By.css('#direccion')); // Selecting the input element
+    inputElement.nativeElement.dispatchEvent(new Event('blur')); // Simulating blur event
+    fixture.detectChanges(); // Updating the component
+
+    const errorMessage = fixture.debugElement.query(By.css('.alert.alert-danger')); // Selecting the error message element
+    expect(errorMessage).toBeTruthy(); // Asserting that the error message exists
+    expect(errorMessage.nativeElement.textContent.trim()).toBe('Por favor diligencie este campo'); // Asserting the content of the error message
+  });
+
+  it('Should have a nombre del propietario text box', () => {
+    const name: HTMLElement = fixture.nativeElement.querySelector('#nombre_propietario');
+    expect(name).toBeTruthy();
+  });
+
+  it('should display error message when nombre del propietario field is touched and empty', () => {
+    const inputElement = fixture.debugElement.query(By.css('#nombre_propietario'));
+    inputElement.nativeElement.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.alert.alert-danger'));
+    expect(errorMessage.nativeElement.textContent.trim()).toBe('Contenido requerido');
+  });
+
+  it('Should have a numero de contacto text box', () => {
+    const name: HTMLElement = fixture.nativeElement.querySelector('#numero_contacto');
+    expect(name).toBeTruthy();
+  });
+
+  it('should display error message when numero de contacto field is touched and empty', () => {
+    const inputElement = fixture.debugElement.query(By.css('#numero_contacto'));
+    inputElement.nativeElement.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+    const errorMessage = fixture.debugElement.query(By.css('.alert.alert-danger'));
+    expect(errorMessage.nativeElement.textContent.trim()).toBe('Contenido requerido');
+  });
+
+  it('Should have a numero de cuenta text box', () => {
+    const name: HTMLElement = fixture.nativeElement.querySelector('#numero_cuenta');
+    expect(name).toBeTruthy();
+  });
+
 });
