070826b096895060ef39fb65cd21fb92360ff64c
DaGamez <da.gamez96@uniandes.edu.co>
2024-02-28 07:09:46 -0500

    PA_15_test: servicio listar propietarios funciona (rojo)

--- src/app/usuario/usuario.service.spec.ts
+++ src/app/usuario/usuario.service.spec.ts
@@ -1,1 +1,40 @@
-//TODO
+//runs with ng test --include src/app/usuario/usuario.service.spec.ts
+
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { UsuarioService } from './usuario.service';
+
+describe('UsuarioService', () => {
+  let service: UsuarioService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsuarioService]
+    });
+    service = TestBed.inject(UsuarioService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it(' listarPropietarios() should return data', () => {
+    const mockPropietarios = [
+      { id: 1, nombre: 'Juan', apellidos: 'Perez', tipoIdentificacion: 'CC', identificacion: '123456789'}
+    ];
+
+    service.listarPropietarios().subscribe(prop => {
+      expect(prop.length).toBe(1);
+      expect(prop).toEqual(mockPropietarios);
+    });
+
+
+  
+});
